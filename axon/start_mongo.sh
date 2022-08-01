0d27e432518e168c10d86447b1d09d65d2a18bec
Gerard Klijs <gerard.klijs@axoniq.io>
2022-08-01 17:44:10 +0200

    Add mongo as projection store

--- axon/start_mongo.sh
+++ axon/start_mongo.sh
@@ -1,6 +1,6 @@
 docker run \
   -d \
-  --name order-projection \
+  --name order_projection \
   -p 27017:27017 \
   -e MONGO_INITDB_ROOT_USERNAME=admin1234 \
   -e MONGO_INITDB_ROOT_PASSWORD=somepassword \
