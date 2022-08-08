a4319c340bac14abd816d5074e7a4464d94dc1ab
Gerard Klijs <gerard.klijs@axoniq.io>
2022-08-08 12:33:05 +0200

    Use a Response object for the endpoint, update Axon bom version, make tests use given when then.

--- axon/src/main/java/com/baeldung/axon/coreapi/queries/TotalProductsShippedQuery.java
+++ axon/src/main/java/com/baeldung/axon/coreapi/queries/TotalProductsShippedQuery.java
@@ -1,5 +1,7 @@
 package com.baeldung.axon.coreapi.queries;
 
+import java.util.Objects;
+
 public class TotalProductsShippedQuery {
     private final String productId;
 
@@ -9,5 +11,28 @@ public class TotalProductsShippedQuery {
 
     public String getProductId() {
         return productId;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TotalProductsShippedQuery that = (TotalProductsShippedQuery) o;
+        return Objects.equals(productId, that.productId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId);
+    }
+
+    @Override
+    public String toString() {
+        return "TotalProductsShippedQuery{" +
+                "productId='" + productId + '\'' +
+                '}';
     }
 }
--- axon/src/test/java/com/baeldung/axon/querymodel/AbstractOrdersEventHandlerUnitTest.java
+++ axon/src/test/java/com/baeldung/axon/querymodel/AbstractOrdersEventHandlerUnitTest.java
@@ -6,6 +6,7 @@ import com.baeldung.axon.coreapi.events.OrderShippedEvent;
 import com.baeldung.axon.coreapi.events.ProductAddedEvent;
 import com.baeldung.axon.coreapi.events.ProductCountDecrementedEvent;
 import com.baeldung.axon.coreapi.events.ProductCountIncrementedEvent;
+import com.baeldung.axon.coreapi.events.ProductRemovedEvent;
 import com.baeldung.axon.coreapi.queries.FindAllOrderedProductsQuery;
 import com.baeldung.axon.coreapi.queries.Order;
 import com.baeldung.axon.coreapi.queries.OrderStatus;
@@ -37,12 +38,8 @@ public abstract class AbstractOrdersEventHandlerUnitTest {
     protected abstract OrdersEventHandler getHandler();
 
     @Test
-    void findAllOrderedProductsQueryTest() {
-        handler.on(new OrderCreatedEvent(ORDER_ID_1));
-        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
-        handler.on(new OrderConfirmedEvent(ORDER_ID_1));
-        handler.on(new OrderCreatedEvent(ORDER_ID_2));
-        handler.on(new ProductAddedEvent(ORDER_ID_2, PRODUCT_ID_2));
+    void givenTwoOrdersPlacedOfWhichOneNotShipped_whenFindAllOrderedProductsQuery_thenCorrectOrdersAreReturned() {
+        createTwoOrdersOnlyShipFirst();
 
         List<Order> result = handler.handle(new FindAllOrderedProductsQuery());
 
@@ -51,41 +48,40 @@ public abstract class AbstractOrdersEventHandlerUnitTest {
 
         Order order_1 = result.stream().filter(o -> o.getOrderId().equals(ORDER_ID_1)).findFirst().orElseThrow();
         assertEquals(1, order_1.getProducts().size());
-        assertEquals(1, order_1.getProducts().get(PRODUCT_ID_1));
-        assertEquals(OrderStatus.CONFIRMED, order_1.getOrderStatus());
+        assertEquals(3, order_1.getProducts().get(PRODUCT_ID_1));
+        assertEquals(OrderStatus.SHIPPED, order_1.getOrderStatus());
 
         Order order_2 = result.stream().filter(o -> o.getOrderId().equals(ORDER_ID_2)).findFirst().orElseThrow();
-        assertEquals(1, order_2.getProducts().size());
+        assertEquals(2, order_2.getProducts().size());
+        assertEquals(1, order_2.getProducts().get(PRODUCT_ID_1));
         assertEquals(1, order_2.getProducts().get(PRODUCT_ID_2));
-        assertEquals(OrderStatus.CREATED, order_2.getOrderStatus());
+        assertEquals(OrderStatus.CONFIRMED, order_2.getOrderStatus());
     }
 
     @Test
-    void totalShippedProductsQueryTest() {
-        handler.on(new OrderCreatedEvent(ORDER_ID_1));
-        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
-        handler.on(new ProductCountIncrementedEvent(ORDER_ID_1, PRODUCT_ID_1));
-        handler.on(new ProductCountIncrementedEvent(ORDER_ID_1, PRODUCT_ID_1));
-        handler.on(new OrderConfirmedEvent(ORDER_ID_1));
-        handler.on(new OrderCreatedEvent(ORDER_ID_2));
-        handler.on(new ProductAddedEvent(ORDER_ID_2, PRODUCT_ID_1));
-        handler.on(new ProductAddedEvent(ORDER_ID_2, PRODUCT_ID_2));
-        handler.on(new OrderConfirmedEvent(ORDER_ID_1));
-
+    void givenNoOrdersPlaced_whenTotalProductsShippedQuery_thenZeroReturned() {
         assertEquals(0, handler.handle(new TotalProductsShippedQuery(PRODUCT_ID_1)));
-        assertEquals(0, handler.handle(new TotalProductsShippedQuery(PRODUCT_ID_2)));
+    }
 
-        handler.on(new OrderShippedEvent(ORDER_ID_1));
+    @Test
+    void givenTwoOrdersPlacedOfWhichOneNotShipped_whenTotalProductsShippedQuery_thenOnlyCountProductsFirstOrder() {
+        createTwoOrdersOnlyShipFirst();
+
         assertEquals(3, handler.handle(new TotalProductsShippedQuery(PRODUCT_ID_1)));
         assertEquals(0, handler.handle(new TotalProductsShippedQuery(PRODUCT_ID_2)));
+    }
 
+    @Test
+    void givenTwoOrdersPlacedAndShipped_whenTotalProductsShippedQuery_thenCountBothOrders() {
+        createTwoOrdersOnlyShipFirst();
         handler.on(new OrderShippedEvent(ORDER_ID_2));
+
         assertEquals(4, handler.handle(new TotalProductsShippedQuery(PRODUCT_ID_1)));
         assertEquals(1, handler.handle(new TotalProductsShippedQuery(PRODUCT_ID_2)));
     }
 
     @Test
-    void orderStatusUpdatesQueryTest(){
+    void givenOrderExist_whenOrderUpdatesQuery_thenOrderReturned(){
         handler.on(new OrderCreatedEvent(ORDER_ID_1));
         handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
 
@@ -94,30 +90,83 @@ public abstract class AbstractOrdersEventHandlerUnitTest {
         assertEquals(ORDER_ID_1, result.getOrderId());
         assertEquals(1, result.getProducts().get(PRODUCT_ID_1));
         assertEquals(OrderStatus.CREATED, result.getOrderStatus());
-        verify(emitter, times(1)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    }
 
-        handler.on(new ProductCountIncrementedEvent(ORDER_ID_1, PRODUCT_ID_1));
-        result = handler.handle(new OrderUpdatesQuery(ORDER_ID_1));
-        assertNotNull(result);
-        assertEquals(2, result.getProducts().get(PRODUCT_ID_1));
-        verify(emitter, times(2)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    @Test
+    void givenOrderExist_whenProductAddedEvent_thenUpdateEmittedOnce(){
+        handler.on(new OrderCreatedEvent(ORDER_ID_1));
+
+        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
+
+        verify(emitter, times(1)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    }
+
+    @Test
+    void givenOrderWithProductExist_whenProductCountDecrementedEvent_thenUpdateEmittedOnce(){
+        handler.on(new OrderCreatedEvent(ORDER_ID_1));
+        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        reset(emitter);
 
         handler.on(new ProductCountDecrementedEvent(ORDER_ID_1, PRODUCT_ID_1));
-        result = handler.handle(new OrderUpdatesQuery(ORDER_ID_1));
-        assertNotNull(result);
-        assertEquals(1, result.getProducts().get(PRODUCT_ID_1));
-        verify(emitter, times(3)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+
+        verify(emitter, times(1)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    }
+
+    @Test
+    void givenOrderWithProductExist_whenProductRemovedEvent_thenUpdateEmittedOnce(){
+        handler.on(new OrderCreatedEvent(ORDER_ID_1));
+        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        reset(emitter);
+
+        handler.on(new ProductRemovedEvent(ORDER_ID_1, PRODUCT_ID_1));
+
+        verify(emitter, times(1)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    }
+
+    @Test
+    void givenOrderWithProductExist_whenProductCountIncrementedEvent_thenUpdateEmittedOnce(){
+        handler.on(new OrderCreatedEvent(ORDER_ID_1));
+        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        reset(emitter);
+
+        handler.on(new ProductCountIncrementedEvent(ORDER_ID_1, PRODUCT_ID_1));
+
+        verify(emitter, times(1)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    }
+
+    @Test
+    void givenOrderWithProductExist_whenOrderConfirmedEvent_thenUpdateEmittedOnce(){
+        handler.on(new OrderCreatedEvent(ORDER_ID_1));
+        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        reset(emitter);
 
         handler.on(new OrderConfirmedEvent(ORDER_ID_1));
-        result = handler.handle(new OrderUpdatesQuery(ORDER_ID_1));
-        assertNotNull(result);
-        assertEquals(OrderStatus.CONFIRMED, result.getOrderStatus());
-        verify(emitter, times(4)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+
+        verify(emitter, times(1)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    }
+
+    @Test
+    void givenOrderWithProductAndConfirmationExist_whenOrderShippedEvent_thenUpdateEmittedOnce(){
+        handler.on(new OrderCreatedEvent(ORDER_ID_1));
+        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        reset(emitter);
 
         handler.on(new OrderShippedEvent(ORDER_ID_1));
-        result = handler.handle(new OrderUpdatesQuery(ORDER_ID_1));
-        assertNotNull(result);
-        assertEquals(OrderStatus.SHIPPED, result.getOrderStatus());
-        verify(emitter, times(5)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+
+        verify(emitter, times(1)).emit(eq(OrderUpdatesQuery.class), any(), any(Order.class));
+    }
+
+    private void createTwoOrdersOnlyShipFirst(){
+        handler.on(new OrderCreatedEvent(ORDER_ID_1));
+        handler.on(new ProductAddedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        handler.on(new ProductCountIncrementedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        handler.on(new ProductCountIncrementedEvent(ORDER_ID_1, PRODUCT_ID_1));
+        handler.on(new OrderConfirmedEvent(ORDER_ID_1));
+        handler.on(new OrderShippedEvent(ORDER_ID_1));
+
+        handler.on(new OrderCreatedEvent(ORDER_ID_2));
+        handler.on(new ProductAddedEvent(ORDER_ID_2, PRODUCT_ID_1));
+        handler.on(new ProductAddedEvent(ORDER_ID_2, PRODUCT_ID_2));
+        handler.on(new OrderConfirmedEvent(ORDER_ID_2));
     }
 }
